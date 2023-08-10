2c19fa14df84aff0bac119566276f93b4aa70243
Spencer Ploeger <43148913+scploeger@users.noreply.github.com>
2023-08-10 13:18:52 -0400

    Update RestaurantsApplication.java

--- restaurants/src/main/java/com/sploeger/restaurants/RestaurantsApplication.java
+++ restaurants/src/main/java/com/sploeger/restaurants/RestaurantsApplication.java
@@ -2,6 +2,8 @@ package com.sploeger.restaurants; // declaring a package
 
 import org.springframework.boot.SpringApplication; // import Spring Application class to use run method
 import org.springframework.boot.autoconfigure.SpringBootApplication; // import Spring Boot application annotation
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class RestaurantsApplication {
@@ -9,5 +11,6 @@ public class RestaurantsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantsApplication.class, args);
 	}
+
 
 }
