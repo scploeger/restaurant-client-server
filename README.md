9e0f0ec4f7ae291db8de4628fc74e85535b29f7b
Spencer Ploeger <43148913+scploeger@users.noreply.github.com>
2023-08-10 11:31:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# restaurant-reviews
+ A restauraunt review application. This project uses MongoDb for the database, Java and Spring Boot for the backend, and React for the frontend.  Based on this guide: https://youtu.be/5PdEmeopJVQ
---
+++ restaurants/src/main/java/com/sploeger/restaurants/RestaurantsApplication.java
@@ -0,0 +1,13 @@
+package com.sploeger.restaurants; // declaring a package
+
+import org.springframework.boot.SpringApplication; // import Spring Application class to use run method
+import org.springframework.boot.autoconfigure.SpringBootApplication; // import Spring Boot application annotation
+
+@SpringBootApplication
+public class RestaurantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantsApplication.class, args);
+	}
+
+}
