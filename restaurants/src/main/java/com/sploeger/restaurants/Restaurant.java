2720c72f8e831fc517cef5b8277c87130425a792
Spencer Ploeger <43148913+scploeger@users.noreply.github.com>
2023-08-21 13:39:59 -0400

    getters, setters, and toString for restaurant class

--- restaurants/src/main/java/com/sploeger/restaurants/Restaurant.java
+++ restaurants/src/main/java/com/sploeger/restaurants/Restaurant.java
@@ -1,12 +1,18 @@
 package com.sploeger.restaurants;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
-@Document(collection = "restaurant") // This tells the framework that this class represents each item in the restaurants collection
+@Document(collection = "restaurants") // This tells the framework that this class represents each item in the restaurants collection
+@Data // from lombok, creates getters, setters and toString methods
+@AllArgsConstructor // creates a constructor from all private fields
+@NoArgsConstructor // creates a constructor with no arguments
 public class Restaurant {
     @Id // let spring framework know that this is the unique identifier for each restaurant
     private ObjectId id;
