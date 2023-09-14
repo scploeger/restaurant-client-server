dcb7504326b548e943d57fe98908212e7f7ddc9a
Spencer Ploeger <43148913+scploeger@users.noreply.github.com>
2023-09-14 14:38:22 -0400

    updated backend

--- restaurants/src/main/java/com/sploeger/restaurants/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sploeger.restaurants;
-
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
-}
